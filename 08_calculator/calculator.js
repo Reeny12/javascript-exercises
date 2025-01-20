37bd48a8bf341c6a594aef5a56b6149b91afcca9
Reeny12 <lebotsamaureen@gmail'com>
2025-01-20 15:51:51 +0200

    Used reduce function to get the sum and product of an array of numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+//console.log(add(15, 20));
+
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+//console.log(subtract(25, 10));
+
+const sum = function(numbers) {
+  return numbers.reduce(function(total, current){
+    return total + current;
+  }, 0)
 };
 
-const multiply = function() {
+//console.log(sum([7, 11]))
 
+
+const multiply = function(numbers) {
+  return numbers.reduce(function(product, num){
+    return product * num
+  }, 1)
 };
 
-const power = function() {
-	
+console.log(multiply([2, 4]))
+
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+//console.log(power(2, 3));
+
+const factorial = function(number) {
+  let product = 1;
+  for (let i = number; i > 0; i--){
+    product = product * i;
+  }
+  return product;
 };
+
+//console.log(factorial(6));
 
 // Do not edit below this line
 module.exports = {
